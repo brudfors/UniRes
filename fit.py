700a6078f74b5c4b4515819dbf863354da72f876
Mikael Brudfors <brudfors@gmail.com>
2020-06-22 16:18:47 +0100

    Fixes to fit.py

--- fit.py
+++ fit.py
@@ -17,8 +17,8 @@ from nires import Settings
 import sys
 import torch
 
-def fit(pth, device, dir_out, dir_nitorch, plot_conv, print_info,
-        reg_scl, show_hyperpar, tolerance, vx):
+def fit(pth, device, dir_out, plot_conv, print_info, reg_scl,
+        show_hyperpar, show_jtv, tolerance, unified_rigid, vx):
 
     # CPU/GPU?
     device = torch.device("cpu" if not torch.cuda.is_available() else device)
@@ -27,18 +27,14 @@ def fit(pth, device, dir_out, dir_nitorch, plot_conv, print_info,
     s = Settings()  # Get default settings
     s.device = device
     if dir_out is not None: s.dir_out = dir_out
-    if dir_nitorch is not None: s.dir_nitorch = dir_nitorch
     if plot_conv is not None: s.plot_conv = plot_conv
     if print_info is not None: s.print_info = print_info
     if reg_scl is not None: s.reg_scl = reg_scl
     if show_hyperpar is not None: s.show_hyperpar = show_hyperpar
+    if show_jtv is not None: s.show_jtv = show_jtv
     if tolerance is not None: s.tolerance = tolerance
     if unified_rigid is not None: s.unified_rigid = unified_rigid
     if vx is not None: s.vx = vx
-
-    if dir_nitorch:
-        # Set path to nitorch
-        sys.path.append(dir_nitorch)
 
     # Init algorithm
     model = Model(pth, s)
@@ -54,7 +50,7 @@ if __name__ == "__main__":
     parser.add_argument("pth",
                         type=str,
                         nargs='+',
-                        help="<Required> Path to subject MRIs/CT (.nii/.nii.gz)")
+                        help="<Required> nibabel compatible path(s) to subject MRIs/CT")
     # Optional arguments
     parser.add_argument("--device",
                         type=str,
@@ -64,18 +60,10 @@ if __name__ == "__main__":
                         type=str,
                         default=None,
                         help="Directory to write output, if None uses same as input (output is prefixed 'y_')")
-    parser.add_argument("--dir_nitorch",
-                        type=str,
-                        default=None,
-                        help="Path to nitorch directory (https://github.com/balbasty/nitorch)")
     parser.add_argument("--plot_conv",
                         type=bool,
                         default=None,
                         help="Use matplotlib to plot convergence in real-time")
-    parser.add_argument("--unified_rigid",
-                        type=bool,
-                        default=None,
-                        help="Do unified rigid registration")
     parser.add_argument("--print_info",
                         type=int,
                         default=None,
@@ -88,14 +76,22 @@ if __name__ == "__main__":
                         type=bool,
                         default=None,
                         help="Use matplotlib to visualise hyper-parameter estimates")
+    parser.add_argument("--show_jtv",
+                        type=bool,
+                        default=None,
+                        help="Show the joint total variation (JTV)")
     parser.add_argument("--tolerance",
                         type=float,
                         default=None,
                         help="Algorithm tolerance, if zero, run to max_iter")
+    parser.add_argument("--unified_rigid",
+                        type=bool,
+                        default=None,
+                        help="Do unified rigid registration")
     parser.add_argument("--vx",
                         type=float,
                         default=None,
                         help="Reconstruction voxel size (if None, set automatically)")
     # Apply
     args = parser.parse_args()
-    run(**vars(args))
+    fit(**vars(args))
