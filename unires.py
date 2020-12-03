b08b6a519af51ee3276088de5ae7c34c20d42ef8
Mikael Brudfors <brudfors@gmail.com>
2020-12-03 11:04:08 +0000

    Refactoring

--- unires.py
+++ unires.py
@@ -91,8 +91,8 @@ if __name__ == "__main__":
     #
     parser.add_argument("--atlas_align",
                         action='store_true',
-                        help="Align images to an atlas space ("
-                             "default=" + str(s.do_atlas_align) + ").")
+                        help="Align images to an atlas space ["
+                             "default=" + str(s.do_atlas_align) + "].")
     parser.add_argument('--no-atlas_align', dest='atlas_align',
                         action='store_false')
     parser.set_defaults(atlas_align=s.do_atlas_align)
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     parser.add_argument("--atlas_rigid",
                         action='store_true',
                         help="Rigid, else rigid+isotropic, alignment to "
-                             "atlas (default=" + str(s.atlas_rigid) + ").")
+                             "atlas [default=" + str(s.atlas_rigid) + "].")
     parser.add_argument('--no-atlas_rigid', dest='atlas_rigid',
                         action='store_false')
     parser.set_defaults(atlas_rigid=s.atlas_rigid)
@@ -108,8 +108,8 @@ if __name__ == "__main__":
     parser.add_argument("--crop",
                         action='store_true',
                         help="Crop input images' FOV to brain in the "
-                             "NITorch atlas (default=" + str(s.crop) +
-                             ").")
+                             "NITorch atlas [default=" + str(s.crop) +
+                             "].")
     parser.add_argument('--no-crop', dest='crop',
                         action='store_false')
     parser.set_defaults(crop=s.crop)
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     parser.add_argument("--device",
                         type=str,
                         default="cuda",
-                        help="PyTorch device (default='cuda').")
+                        help="PyTorch device [default='cuda'].")
     #
     parser.add_argument("--dir_out",
                         type=str,
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     parser.add_argument("--linear",
                         action='store_true',
                         help="Reslice using trilinear interpolation, i.e.,"
-                             "no super-resolution (default=False).")
+                             "no super-resolution [default=False].")
     parser.add_argument('--no-linear', dest='linear',
                         action='store_false')
     parser.set_defaults(linear=False)
@@ -136,8 +136,8 @@ if __name__ == "__main__":
     parser.add_argument("--plot_conv",
                         action='store_true',
                         help="Use matplotlib to plot convergence in "
-                             "real-time (default=" + str(s.plot_conv) +
-                             ").")
+                             "real-time [default=" + str(s.plot_conv) +
+                             "].")
     parser.add_argument('--no-plot_conv', dest='plot_conv',
                         action='store_false')
     parser.set_defaults(plot_conv=s.plot_conv)
@@ -145,25 +145,25 @@ if __name__ == "__main__":
     parser.add_argument("--prefix",
                         type=str,
                         default=s.prefix,
-                        help="Output image(s) prefix (default=" + str(
-                            s.prefix) + ").")
+                        help="Output image(s) prefix [default=" + str(
+                            s.prefix) + "].")
     #
     parser.add_argument("--print_info",
                         type=int,
                         default=s.do_print,
-                        help="Print progress to terminal (0, 1, 2; "
-                             "default=" + str(s.do_print) + ").")
+                        help="Print progress to terminal [0, 1, 2; "
+                             "default=" + str(s.do_print) + "].")
     #
     parser.add_argument("--reg_scl",
                         type=float,
                         default=s.reg_scl,
-                        help="Scale regularisation estimate (default=" +
-                             str(s.reg_scl) + ").")
+                        help="Scale regularisation estimate [default=" +
+                             str(s.reg_scl) + "].")
     #
     parser.add_argument("--res_origin",
                         action='store_true',
-                        help="Resets origin, if CT data (default=" +
-                             str(s.do_res_origin) + ").")
+                        help="Resets origin, if CT data [default=" +
+                             str(s.do_res_origin) + "].")
     parser.add_argument('--no-res_origin', dest='res_origin',
                         action='store_false')
     parser.set_defaults(res_origin=s.do_res_origin)
@@ -171,22 +171,22 @@ if __name__ == "__main__":
     parser.add_argument("--sched",
                         type=int,
                         default=s.sched_num,
-                        help="Number of coarse-to-fine scalings ("
-                             "default=" + str(s.sched_num) + ").")
+                        help="Number of coarse-to-fine scalings ["
+                             "default=" + str(s.sched_num) + "].")
     #
     parser.add_argument("--show_hyperpar",
                         action='store_true',
                         help="Use matplotlib to visualise "
-                             "hyper-parameter estimates (default=" +
-                             str(s.show_hyperpar) + ").")
+                             "hyper-parameter estimates [default=" +
+                             str(s.show_hyperpar) + "].")
     parser.add_argument('--no-show_hyperpar', dest='show_hyperpar',
                         action='store_false')
     parser.set_defaults(show_hyperpar=s.show_hyperpar)
     #
     parser.add_argument("--show_jtv",
                         action='store_true',
-                        help="Show the joint total variation ("
-                             "default=" + str(s.show_jtv) + ").")
+                        help="Show the joint total variation ["
+                             "default=" + str(s.show_jtv) + "].")
     parser.add_argument('--no-show_jtv', dest='show_jtv',
                         action='store_false')
     parser.set_defaults(show_jtv=s.show_jtv)
@@ -195,13 +195,13 @@ if __name__ == "__main__":
                         type=float,
                         default=s.tolerance,
                         help="Algorithm tolerance, if zero, run to "
-                             "max_iter (default=" + str(s.tolerance) +
-                             ").")
+                             "max_iter [default=" + str(s.tolerance) +
+                             "].")
     #
     parser.add_argument("--unified_rigid",
                         action='store_true',
-                        help="Do unified rigid registration ("
-                             "default=" + str(s.unified_rigid) + ").")
+                        help="Do unified rigid registration ["
+                             "default=" + str(s.unified_rigid) + "].")
     parser.add_argument('--no-unified_rigid', dest='unified_rigid',
                         action='store_false')
     parser.set_defaults(unified_rigid=s.unified_rigid)
@@ -209,13 +209,13 @@ if __name__ == "__main__":
     parser.add_argument("--vx",
                         type=float,
                         default=s.vx,
-                        help="Reconstruction voxel size (default=" +
-                             str(s.vx) + ").")
+                        help="Reconstruction voxel size [default=" +
+                             str(s.vx) + "].")
     #
     parser.add_argument("--write_out",
                         action='store_true',
-                        help="Write reconstructed output images ("
-                             "default=" + str(s.write_out) + ").")
+                        help="Write reconstructed output images ["
+                             "default=" + str(s.write_out) + "].")
     parser.add_argument('--no-write_out', dest='write_out',
                         action='store_false')
     parser.set_defaults(write_out=s.write_out)
