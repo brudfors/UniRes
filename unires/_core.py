* Added new setting sett.fix that sets the fixed image index for atlas alignment (default=0) * Default regularisation scaling set to 1 * Even/odd correction disabled by default

unires/_core.py
@@ -316,7 +316,7 @@ def _init_reg(x, sett):
     # Set rigid affine basis
     sett.rigid_basis = affine_basis(
         group='SE', device=sett.device, dtype=torch.float64)
-    fix = 0  # Fixed image index
+    fix =sett.fix  # Fixed image index
 
     # Make input for nitorch affine align
     imgs = []